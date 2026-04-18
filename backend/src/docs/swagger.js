469659fe3d29f458205e5e3d6a4a2905fa0b6a6a
anshu0714 <anshujha14072002@gmail.com>
2026-04-18 18:16:43 +0530

    docs(update): update swagger file for production only

--- backend/src/docs/swagger.js
+++ backend/src/docs/swagger.js
@@ -2,10 +2,8 @@ const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const path = require("path");
 
-// Load YAML file
 const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
 
-// Setup function
 const setupSwagger = (app) => {
   app.use(
     "/api-docs",
